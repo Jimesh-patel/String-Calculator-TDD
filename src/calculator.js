7e62e80a6762e8f3d6f97a27890ea20b0c46e5a7
Jimesh-patel <vadodariyajimesh1811@gmaiil.com>
2025-07-05 00:51:32 +0530

    [#5.1] feat : support custom single-character delimiter

--- src/calculator.js
+++ src/calculator.js
@@ -4,12 +4,18 @@ class StringCalculator {
     add(numbers) {
         if (!numbers) return 0;
 
+        let delimiter = /[\n,]/;
+        if (numbers.startsWith('//')) {
+            const delimiterLine = numbers.split('\n')[0];
+            delimiter = new RegExp(numbers[2]);  
+            numbers = numbers.split('\n')[1];
+        }
+
         return numbers
-            .split(/[\n,]/)   
+            .split(delimiter)
             .map(Number)
             .reduce((sum, n) => sum + n, 0);
-
-    }
+    }      
       
 }
 
