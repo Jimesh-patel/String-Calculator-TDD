cdbb890c917cdd3fc363b989d4934207f3cf3e50
Jimesh-patel <vadodariyajimesh1811@gmaiil.com>
2025-07-05 00:26:49 +0530

    [#2.0] test : fail for single number input

--- tests/calculator.test.js
+++ tests/calculator.test.js
@@ -1,8 +1,15 @@
 const StringCalculator = require('../src/calculator');
 
 describe('String Calculator', () => {
+
     test('should return 0 for an empty string', () => {
         const calc = new StringCalculator();
         expect(calc.add("")).toBe(0);
     });
+
+    test('should return the number itself if a single number is provided', () => {
+        const calc = new StringCalculator();
+        expect(calc.add("2")).toBe(2); 
+    });
+      
 });
