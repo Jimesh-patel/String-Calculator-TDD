ed8e4d69e292c3c2a583c06d96a555d90ac178fd
Jimesh-patel <vadodariyajimesh1811@gmaiil.com>
2025-07-05 15:13:27 +0530

    [#11.2] feat : correctly parse and match full multi-character delimiters

--- src/helper/delimiterParser.js
+++ src/helper/delimiterParser.js
@@ -1,10 +1,30 @@
 function parseDelimiter(input) {
     if (input.startsWith('//')) {
         const [delimiterLine, numbers] = input.split('\n');
-        const delimiter = delimiterLine.slice(2);
-        return { delimiter: new RegExp(delimiter), numbers };
+
+        const delimiterMatches = delimiterLine.match(/\[(.+)\]/);
+
+        if (delimiterMatches) {
+            const rawDelimiter = delimiterMatches[1];
+            const escapedDelimiter = escapeRegExp(rawDelimiter); 
+            return {
+                delimiter: new RegExp(escapedDelimiter),
+                numbers,
+            };
+        }
+
+        const singleDelimiter = delimiterLine.slice(2);
+        return {
+            delimiter: new RegExp(escapeRegExp(singleDelimiter)),
+            numbers,
+        };
     }
+
     return { delimiter: /[\n,]/, numbers: input };
+}
+
+function escapeRegExp(str) {
+    return str.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
 }
 
 module.exports = { parseDelimiter };
